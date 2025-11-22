7a1fe2693cf12dff311bfa4ca9ff9a874a0c1af0
ali6eza <tayefehalireza@gmail.com>
2025-11-22 22:08:41 +0330

    First commit

---
+++ src/main/java/com/cinema/ticketing/TicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemApplication.class, args);
+	}
+
+}
